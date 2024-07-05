b2964ea73dd665793d0593590325f93b7067ec44
Usopp <gwee1216@naver.com>
2024-07-05 10:48:12 +0900

    init Project

---
+++ src/test/java/com/usopp/ediary/EdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usopp.ediary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
